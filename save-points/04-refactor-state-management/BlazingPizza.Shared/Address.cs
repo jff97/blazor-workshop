9218e37d06936ca8ff1ecb250fad018ceeac4b86
John Fox <107318784+jff97@users.noreply.github.com>
2023-06-02 16:21:41 -0500

    login deprecated fix next

--- save-points/04-refactor-state-management/BlazingPizza.Shared/Address.cs
+++ save-points/04-refactor-state-management/BlazingPizza.Shared/Address.cs
@@ -5,16 +5,22 @@ namespace BlazingPizza;
 public class Address
 {
     public int Id { get; set; }
-
+    
+    [Required]
     public string Name { get; set; }
-
+    
+    [Required]
     public string Line1 { get; set; }
-
+    
+    [Required]
     public string Line2 { get; set; }
-
+    
+    [Required]
     public string City { get; set; }
-
+    
+    [Required]
     public string Region { get; set; }
-
+    
+    [Required]
     public string PostalCode { get; set; }
 }
